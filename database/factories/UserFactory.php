212b4a3bf25a7952309ec0d1202ecea76b80337d
Sagar Shiroya <sagar.shiroya8@gmail.com>
2022-02-18 17:30:44 +0530

    Added user factory and added to generate 10 users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,18 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'uuid' => Str::uuid(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'is_admin' => 0,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'avatar' => null,
+            'address' => $this->faker->address(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'is_marketing' => 0,
+            'last_login_at' => now()
         ];
     }
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,14 +18,14 @@ class CreateUsersTable extends Migration
             $table->uuid('uuid');
             $table->string('first_name', 255);
             $table->string('last_name', 255);
-            $table->tinyInteger('is_admin')->default(0);
+            $table->boolean('is_admin')->default(0);
             $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 255);
             $table->char('avatar', 36)->nullable();
             $table->string('address', 255);
             $table->string('phone_number', 255);
-            $table->tinyInteger('is_marketing')->default(0);
+            $table->boolean('is_marketing')->default(0);
             $table->timestamps();
             $table->timestamp('last_login_at')->nullable();
         });
